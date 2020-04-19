c6231c01afea0c660affa1105e47a75f7457b6ca
Animatt546 <59981155+Animatt546@users.noreply.github.com>
2020-04-19 21:59:56 +0200

    Pojawianie

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const bryly = document.getElementsByClassName('bryla');
 for (let i = 0; i < bryly.length; i++) {
     bryly[i].addEventListener("click", function() {
         bryly[i].classList.add('hidden');
+        setTimeout(function(){
+            bryly[i].classList.remove('hidden');
+        }, 3000);
     });
 }
